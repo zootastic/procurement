93ad81d5c7a3a377ba0d1dfa5e0c50dd42544f12
stickymaddness <stickymaddness@gmail.com>
2014-03-20 12:30:28 +0000

    Added White displayColorMapping - Imprints

--- Procurement/ViewModel/DisplayModeStrategy/DisplayModeStrategyBase.cs
+++ Procurement/ViewModel/DisplayModeStrategy/DisplayModeStrategyBase.cs
@@ -19,6 +19,7 @@ namespace Procurement.ViewModel
             displayColorMappings.Add(5, new SolidColorBrush((Color)ColorConverter.ConvertFromString("#2943c6"))); //Blue cold
             displayColorMappings.Add(6, new SolidColorBrush((Color)ColorConverter.ConvertFromString("#f2bc01"))); //Yellow lightning
             displayColorMappings.Add(7, new SolidColorBrush((Color)ColorConverter.ConvertFromString("#D02090"))); //Pink chaos
+            displayColorMappings.Add(9, new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF"))); //White
             
         }
 
